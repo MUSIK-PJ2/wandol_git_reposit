11c6f7929f779cc02c13f264ee5b72af9ada87b2
JungHK <antkdi@gmail.com>
2015-05-09 18:21:50 +0900

    1111111

--- spring/src/main/java/com/java/ex/MainClass.java
+++ spring/src/main/java/com/java/ex/MainClass.java
@@ -12,6 +12,9 @@ import org.springframework.core.io.support.ResourcePropertySource;
 public class MainClass {
 	public static void main(String[] args) {
 		System.out.println(1);
+		System.out.println(1);
+		System.out.println(1);
+		System.out.println(1);
 		ConfigurableApplicationContext ctx = new GenericXmlApplicationContext();
 		ConfigurableEnvironment env = ctx.getEnvironment();
 		MutablePropertySources pro = env.getPropertySources();
